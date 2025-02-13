4ddf444077e3c8439371d5a3a2b0631ddebc7377
Ivan Grega <grega.ivan@gmail.com>
2025-02-13 11:19:50 +0000

    Clean up scripts

--- scripts/load_datasets.py
+++ scripts/load_datasets.py
@@ -18,7 +18,7 @@ base_names = set()
 all_num_edges = []
 all_num_nodes = []
 for i in range(5):
-    f = Path(f"E:/Dropbox (Cambridge University) (Old)/neural-networks/GLAMM/rad_dset_0/cat_{i:02d}_rad.lat")
+    f = Path(f"./cat_{i:02d}_rad.lat")
     cat = Catalogue.from_file(f, 0, regex='.*_p_0.0_.*') # no imperfections
     for data in tqdm(cat):
         name = data['name']
@@ -29,14 +29,15 @@ for i in range(5):
             all_num_edges.append(num_edges)
             all_num_nodes.append(num_nodes)
             base_names.add(bn)
-print(len(base_names)) # 8723 -- filtered
+print(len(base_names))
 base_names = list(base_names)
 # %%
 df = pd.DataFrame({'num_edges': all_num_edges, 'num_nodes': all_num_nodes})
 df.describe()
 # %% Split base names into train, val, test approx 80/10/10
 n = len(base_names)
-n_train, n_val = 3600, 450
+n_train = int(0.8*n)
+n_val = int(0.1*n)
 n_test = n-n_train-n_val
 random.shuffle(base_names)
 train = base_names[:n_train]
@@ -49,7 +50,7 @@ Path('test.txt').write_text('\n'.join(test))
 # %% Assemble train catalogue
 cat_dicts = {'train':{}, 'val':{}, 'test':{}}
 for i in range(5):
-    f = Path(f"E:/Dropbox (Cambridge University) (Old)/neural-networks/GLAMM/rad_dset_0/cat_{i:02d}_rad.lat")
+    f = Path(f"./cat_{i:02d}_rad.lat")
     cat = Catalogue.from_file(f, 0)
     for data in tqdm(cat):
         name = data['name']
